6f0e94ddbf4d62d5c4868ab534da1a5a2858c7ff
angcyo <angcyo@126.com>
2016-09-22 23:57:26 +0800

    2016-9-22

--- src/com/angcyo/Controller1.java
+++ src/com/angcyo/Controller1.java
@@ -1,10 +1,7 @@
 package com.angcyo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -25,7 +22,7 @@ public class Controller1 {
     @RequestMapping("vc/de")
     @ResponseBody
     public String demo2(HttpServletRequest request) {
-        System.out.println("demo..." + request.getMethod());
+        System.out.println("demo..." + request.getMethod() + "  body" + request.getMethod());
         return "{\"Demo2222222\":\"1222222222222222\"}";
     }
 
@@ -41,6 +38,13 @@ public class Controller1 {
     public String demo4(@RequestParam String a, @RequestParam String b) {
         System.out.println("arg... demo..." + a + " --> " + b);
         return "{\"demo4\":\"----aaa--bb\"}";
+    }
+
+    @RequestMapping("/arg2")
+    @ResponseBody
+    public String demo5(HttpServletRequest request, @RequestBody String body) {
+        System.out.println(request.getMethod() + " --> " + " demo5..." + body);
+        return request.getMethod() + " --> " + body;
     }
 }
 
